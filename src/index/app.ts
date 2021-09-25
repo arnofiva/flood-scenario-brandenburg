6215c70224eeb0c9603715a5463692323927cea0
Arno Fiva <arnofiva@gmail.com>
2021-09-25 10:46:10 +0200

    Added initial flood risk and water layers

--- src/index/app.ts
+++ src/index/app.ts
@@ -1,14 +1,60 @@
 import "./style.scss";
 
 import Map = require("esri/Map");
-import SceneView = require("esri/views/SceneView");
+import MapView = require("esri/views/MapView");
+import ElevationLayer = require("esri/layers/ElevationLayer");
+import FeatureLayer = require("esri/layers/FeatureLayer");
+import { SimpleRenderer } from "esri/renderers";
+import { PolygonSymbol3D, FillSymbol3DLayer } from "esri/symbols";
 
 const map = new Map({
-  basemap: "satellite",
-  ground: "world-elevation"
+  basemap: "topo-vector"
 });
 
-const view = new SceneView({
+const view = new MapView({
   container: "viewDiv",
   map
 });
+
+const floodRisk = new FeatureLayer({
+  portalItem: {
+    id: "7b18fa3bae7d47d1ad3344a8d52eb295"
+  }
+});
+map.add(floodRisk);
+
+const lakes = new FeatureLayer({
+  portalItem: {
+    id: "f1b50ae2711c4699b34b485030cfe7ef"
+  }
+});
+map.add(lakes);
+
+const elevation = new ElevationLayer({
+  portalItem: {
+    id: "7029fb60158543ad845c7e1527af11e4"
+  }
+});
+
+view.on("click", async (e) => {
+  const pointOnMap = view.toMap(e);
+
+  const lv = await view.whenLayerView(floodRisk);
+
+  const query = lv.createQuery();
+  query.geometry = pointOnMap; // the point location of the pointer
+  query.distance = 2;
+  query.units = "miles";
+  query.spatialRelationship = "intersects";
+
+  const result = await lv.queryFeatures(query);
+
+  console.log("Medium flood risks", result);
+
+  await elevation.load();
+  const elevationResult = await elevation.queryElevation(pointOnMap);
+
+  console.log("Elevation", elevationResult);
+});
+
+view.popup.defaultPopupTemplateEnabled = true;
